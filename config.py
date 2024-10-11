d5f282c7b1dbcf2c11043b6bc7869b53e70a07bd
FTM DEVELOPER <106005123+ftmdeveloperz@users.noreply.github.com>
2024-10-11 08:41:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
 import os
 import logging
 class Config:                                                                   
-    API_ID = int(os.environ.get("API_ID", ""))
-    API_HASH = os.environ.get("API_HASH", "")       
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    BOT_SESSION = os.environ.get("BOT_SESSION", "forwardbot")
-    OWNER_ID = os.environ.get("OWNER_ID", "")                             
-    DATABASE_URI = os.environ.get("DATABASE_URI", "")  
+    API_ID = environ.get("API_ID", "28776072")
+    API_HASH = environ.get("API_HASH", "b3a786dce1f4e7d56674b7cadfde3c9d")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "7880853237:AAHHyid1rYK8XSE7ZzWpMgqechgutpHK4N0") 
+    BOT_SESSION = environ.get("BOT_SESSION", "bot") 
+    OWNER_ID = os.environ.get("OWNER_ID", "7711039923")                             
+    DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://ftmserver:ftm@cluster0.fneio.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")  
     DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluste0")
     COLLECTION_NAME = os.environ.get('COLLECTION_NAME', 'Data')
-    SESSION = os.environ.get("SESSION", "")   
-    TO_CHANNEL = int(os.environ.get("TO_CHANNEL", ""))
-    BOT_USERNAME= os.environ.get("BOT_USERNAME", "")
+    SESSION = os.environ.get("SESSION", "BQG3FogAJrNnH9YIJY2UYgixF9kTuwwt2AmaWcDll5wHdUlQRop7S6-6IxU2YnIlyWDNkh1VkTxENKvMVoFdziK_lRwdPEXDGFugXm1mu8A8DKQjo9isGbsRsUKUSOcaUAVtAEyNEJN_UePRA9kfHiwdsB13sUp1hTautS5Y2tUacey7Up8l-OMej9tAqb6ixMDTu7WeykFNqMYGpvzO3JD8ClWXyiV8hek15N7ctwlIAo8VhkghqjmwgzhUOHVKdshgHcT9OVKCsdBFxevice0jRXVwbkyGc6MpjwRd1Il4R0IRwbP623tDCmOli-84stKLeNgfRkBZe-n_T3VCIhLyWzmCdAAAAAHLnSGzAA")   
+    TO_CHANNEL = int(os.environ.get("TO_CHANNEL", "-1002261985371"))
+    BOT_USERNAME= os.environ.get("BOT_USERNAME", "ftmcompressorbot")
 
 
 def LOGGER(name: str) -> logging.Logger:
